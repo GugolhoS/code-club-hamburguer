c468099658ab62aaf3e3e178340daf5d12092466
Gustavo Lima dos Santos <92238035+GugolhoS@users.noreply.github.com>
2024-02-24 15:59:24 -0300

    Create CategoryController
    criei o banco de dados de categorias, e fiz a validação para que n trave o sistema caso tente cadastrar duas categorias do mesmo nome

--- Back-end/src/routes.js
+++ Back-end/src/routes.js
@@ -6,6 +6,7 @@ import authMiddleware from './app/middleware/auth'
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
 import ProductController from "./app/controllers/ProductController";
+import CategoryController from "./app/controllers/CategoryController";
 
 const routes = new Router();
 const upload = multer(multerConfig)
@@ -16,5 +17,8 @@ routes.post("/sessions", SessionController.store)
 routes.use(authMiddleware)
 routes.post("/products", upload.single('file'), ProductController.store)
 routes.get('/products', ProductController.index)
+
+routes.post("/categories", CategoryController.store)
+routes.get('/categories', CategoryController.index)
 
 export default routes;
